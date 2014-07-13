859e28be5f1aac44b86d888125e96baf3b42b18b
monai <monai@cure.lt>
2014-07-13 12:20:15 +0300

    meaningful names

--- spec/native.js
+++ spec/native.js
@@ -4,59 +4,59 @@ var assert = require('assert');
 describe('native addon', function () {
     // describe('search(imgMatrix, tplMatrix, colorTolerance, pixelTolerance, callback)', function () {
     describe('matrix object check', function () {
-        function testMissingProperty(regex, img, tpl) {
+        function testError(regex, img, tpl) {
             assert.throws(function () {
                 search(img, tpl);
             }, regex);
         }
         
-        it('should throw error if 1st argument is not passed', function () {
-            testMissingProperty(/Bad argument 'imgMatrix'/);
+        it('should throw error if "imgMatrix" is not passed', function () {
+            testError(/Bad argument 'imgMatrix'/);
         });
         
-        it('should throw error if 1st argument does not have property "rows"', function () {
-            testMissingProperty(/Bad argument 'imgMatrix'/, { cols: 0, channels: 0, data: null });
+        it('should throw error if "imgMatrix" does not have property "rows"', function () {
+            testError(/Bad argument 'imgMatrix'/, { cols: 0, channels: 0, data: null });
         });
         
-        it('should throw error if 1st argument does not have property "cols"', function () {
-            testMissingProperty(/Bad argument 'imgMatrix'/, { rows: 0, channels: 0, data: null });
+        it('should throw error if "imgMatrix" does not have property "cols"', function () {
+            testError(/Bad argument 'imgMatrix'/, { rows: 0, channels: 0, data: null });
         });
         
-        it('should throw error if 1st argument does not have property "channels"', function () {
-            testMissingProperty(/Bad argument 'imgMatrix'/, { rows: 0, cols: 0, data: null });
+        it('should throw error if "imgMatrix" does not have property "channels"', function () {
+            testError(/Bad argument 'imgMatrix'/, { rows: 0, cols: 0, data: null });
         });
         
-        it('should throw error if 1st argument does not have property "data"', function () {
-            testMissingProperty(/Bad argument 'imgMatrix'/, { rows: 0, cols: 0, channels: 0 });
+        it('should throw error if "imgMatrix" does not have property "data"', function () {
+            testError(/Bad argument 'imgMatrix'/, { rows: 0, cols: 0, channels: 0 });
         });
         
-        it('should throw error if 2nd argument is not passed', function () {
-            testMissingProperty(/Bad argument 'tplMatrix'/, { rows: 0, cols: 0, channels: 0, data: null });
+        it('should throw error if "tplMatrix" is not passed', function () {
+            testError(/Bad argument 'tplMatrix'/, { rows: 0, cols: 0, channels: 0, data: null });
         });
         
-        it('should throw error if 2nd argument does not have property "rows"', function () {
-            testMissingProperty(/Bad argument 'tplMatrix'/,
+        it('should throw error if "tplMatrix" does not have property "rows"', function () {
+            testError(/Bad argument 'tplMatrix'/,
                 { rows: 0, cols: 0, channels: 0, data: null },
                 { cols: 0, channels: 0, data: null }
             );
         });
         
-        it('should throw error if 2nd argument does not have property "cols"', function () {
-            testMissingProperty(/Bad argument 'tplMatrix'/,
+        it('should throw error if "tplMatrix" does not have property "cols"', function () {
+            testError(/Bad argument 'tplMatrix'/,
                 { rows: 0, cols: 0, channels: 0, data: null },
                 { rows: 0, channels: 0, data: null }
             );
         });
         
-        it('should throw error if 2nd argument does not have property "channels"', function () {
-            testMissingProperty(/Bad argument 'tplMatrix'/,
+        it('should throw error if "tplMatrix" does not have property "channels"', function () {
+            testError(/Bad argument 'tplMatrix'/,
                 { rows: 0, cols: 0, channels: 0, data: null },
                 { rows: 0, cols: 0, data: null }
             );
         });
         
-        it('should throw error if 2nd argument does not have property "data"', function () {
-            testMissingProperty(/Bad argument 'tplMatrix'/,
+        it('should throw error if "tplMatrix" does not have property "data"', function () {
+            testError(/Bad argument 'tplMatrix'/,
                 { rows: 0, cols: 0, channels: 0, data: null },
                 { rows: 0, cols: 0, channels: 0 }
             );
