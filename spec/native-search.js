86c0fc1ad7d69a3f2347a231c0fa82cccc8642bf
monai <monai@cure.lt>
2014-07-14 12:58:47 +0300

    added native search tests

---
+++ spec/native-search.js
@@ -0,0 +1,8 @@
+var search = require('../build/Release/search').search;
+var assert = require('assert');
+
+describe('native addon - search', function () {
+    it('should work', function () {
+        
+    });
+});
